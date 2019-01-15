09d4ab23bb6ecf6f61933a4145d95f65eef288aa
Dan Binebrink <dan.binebrink@optotraffic.com>
2019-01-15 15:26:13 -0500

    second commit with new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { SidebarComponent } from './main/sidebar/sidebar.component';
+import { SearchComponent } from './main/search/search.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    MainComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent,
+    ProductsComponent,
+    HomeComponent,
+    SidebarComponent,
+    SearchComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  navitems = [
+    {
+      label: 'Home',
+      url: '/home',
+      title: 'Go to Home page'
+    },
+    {
+      label: 'Products',
+      url: '/products',
+      title: 'Go to Products page'
+    },
+    {
+      label: 'About',
+      url: '/about',
+      title: 'Go to About page'
+    },
+    {
+      label: 'Contact',
+      url: '/contact',
+      title: 'Go to Contact page'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  runAlert(): void {
+    alert('yo this is a click event');
+  }
+
+}
