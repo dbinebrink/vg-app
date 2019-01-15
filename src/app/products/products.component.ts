615f83056cc5104d95541b45c4f596eb601963df
Dan Binebrink <dan.binebrink@optotraffic.com>
2019-01-15 16:10:11 -0500

    more minor updates

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+	_searchFilter: string;
+	get searchFilter(): string {
+		return this._searchFilter;
+	}
+	set searchFilter(value:string) {
+		this._searchFilter = value;
+		this.filteredProducts = this._searchFilter ? this.performFilter(this._searchFilter) : this.products;
+	}
+
+
   constructor() { }
 
   ngOnInit() {
