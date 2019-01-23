f5821c729d70af8ed62b4a310b2b2cf439abfde4
Dan Binebrink <dbinebrink@gmail.com>
2019-01-22 19:22:14 -0500

    cleaning up user service

--- src/app/user-list/user-list.service.ts
+++ src/app/user-list/user-list.service.ts
@@ -1,37 +1,21 @@
 import { Injectable } from '@angular/core';
 
 import { IUserList } from './user-list';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
-})  
+})
 export class UserListService {
 
     private userListUrl = 'https://reqres.in/api/users?per_page=10';  // the IUserList INTERFACE has to match this
 
-    constructor(private http: HttpClient) {
-    }
+    constructor(private http: HttpClient) { }
 
-    // the meat of this service is this method, the first two lines are all that matter, get and return
     getUserList(): Observable<IUserList[]> {
-        return this.http.get<IUserList[]>(this.userListUrl).pipe(
-            // tap(data => console.log('JSON response: ' + JSON.stringify(data))),  // TAP() function here to show you the JSON in console.log, TAP into the GOD STREAM
-            catchError(this.handleError)
-        );
+        return this.http.get<IUserList[]>(this.userListUrl);
     }
 
-    private handleError(err: HttpErrorResponse) {
-        let errorMessage = '';
-        if(err.error instanceof ErrorEvent) {
-            errorMessage = `An error occurred: ${err.error.message}`;
-        } else {
-            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
-        }
-        console.error(errorMessage);
-        return throwError(errorMessage);
-    }
 }
 
